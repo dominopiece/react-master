15d46798e8320ea20d89600f64959414d09a12fb
dominopiece <dominopiece@gmail.com>
2023-02-21 23:49:41 +0900

    as, attrs - styled-components

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -9,15 +9,32 @@ function App() {
     width: 100px;
     height: 100px;
   `;
-
   const Circle = styled(Box)`
     border-radius: 50px;
+  `;
+  const Btn = styled.button`
+    color: white;
+    background-color: pink;
+    border: 0;
+    border-radius: 15px;
+  `;
+  const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: pink;
   `;
 
   return (
     <Fater>
-      <Box bgColor="black" />
-      <Circle bgColor="pink" />
+      {/* <Box bgColor="black" /> */}
+      {/* <Circle bgColor="pink" /> */}
+      {/* <Btn>Log in</Btn>
+      <Btn as="a" href="https://www.google.com/">Log in</Btn>
+      <Btn as="h1">Log in</Btn> */}
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Fater>
   );
 }
