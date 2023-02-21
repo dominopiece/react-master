56475147881b32d59a05ed6f65c8bc17a6834bdb
dominopiece <dominopiece@gmail.com>
2023-02-21 23:24:23 +0900

    install & test styled-components

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,8 +1,24 @@
+import styled from "styled-components";
+
 function App() {
+  const Fater = styled.div`
+    display: flex;
+  `;
+  const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
+    width: 100px;
+    height: 100px;
+  `;
+
+  const Circle = styled(Box)`
+    border-radius: 50px;
+  `;
+
   return (
-    <div>
-      setup
-    </div>
+    <Fater>
+      <Box bgColor="black" />
+      <Circle bgColor="pink" />
+    </Fater>
   );
 }
 
