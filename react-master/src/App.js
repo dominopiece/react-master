889831ca08acd9f4b8984f7410abf03ab5c7cb08
dominopiece <dominopiece@gmail.com>
2023-02-23 23:13:20 +0900

    ThemeProvider #3 - styled-components

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -24,6 +24,7 @@ function App() {
   50% {
     transform: rotate(180deg);
     border-radius: 100px;
+    background-color: black;
   }
   100% {
     transform: rotate(360deg);
@@ -38,7 +39,8 @@ function App() {
   const Box = styled.div`
     width: 200px;
     height: 200px;
-    background-color: pink;
+    /* background-color: pink; */
+    background-color: ${(props) => props.bgColor};
     animation: ${rotatioAnimation} 9s linear infinite;
     display: flex;
     justify-content: center;
@@ -56,20 +58,59 @@ function App() {
     }
   `;
 
+  // const Circle = styled(Box)`
+  //   border-radius: 50px;
+  // `;
+
   // const Test1 = styled.span`
   //   font-size: 38px;
   // `;
 
+  const Btn = styled.button`
+    background-color: pink;
+    color: white;
+    width: 90px;
+    height: 20px;
+  `;
+  const Input = styled.input.attrs({ required: true })`
+    background-color: pink;
+  `;
+  const WrapperH1 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    background-color: ${(props) => props.theme.backgroundColor};
+  `;
+  const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
-    <Wrapper>
-      <Box>
-        {/* <Test1>🤺</Test1> */}
-        {/* <span>🤺</span> */}
-        <Emoji as="p">🤺</Emoji>
-        <Emoji>🤺</Emoji>
-      </Box>
-      <Emoji>🤺</Emoji>
-    </Wrapper>
+    <WrapperH1>
+      <Title>textColor</Title>
+    </WrapperH1>
+    // <Wrapper>
+    //   <Box bgColor="pink">
+    //     {/* <Test1>🤺</Test1> */}
+    //     {/* <span>🤺</span> */}
+    //     <Emoji as="p">🤺</Emoji>
+    //     <Emoji>🤺</Emoji>
+    //   </Box>
+    //   <Box bgColor="black" />
+    //   <Emoji>🤺</Emoji>
+    //   {/* <Circle bgColor="pink"/> */}
+    //   <Btn href="https://www.google.com">test</Btn>
+    //   <Btn as="a" href="https://www.google.com">google</Btn>
+    // </Wrapper>
+    // <Wrapper>
+    //   {/* <Input />
+    //   <Input />
+    //   <Input />
+    // <Input /> */}
+    // <Box bgColor="pink">
+
+    // </Box>
+    // </Wrapper>
   );
 }
 
