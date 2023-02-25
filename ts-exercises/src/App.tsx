f9da99fa0a3303c96a76bd9a36e9312f38f5be2f
dominopiece <dominopiece@gmail.com>
2023-02-25 23:59:36 +0900

    study ts #1

--- ts-exercises/src/App.tsx
+++ ts-exercises/src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setUsername(username);
+  };
   return (
     <div>
-      <Circle borderColor="white" bgColor="pink" />
-      <Circle text="App text" bgColor="tomato" />
+      <form>
+        <input
+          onChange={onChange}
+          value={username}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
